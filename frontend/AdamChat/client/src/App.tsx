bbd93e4d585d1fc5e3294e48c746da00d4834d17
vitoryago <vcarvalho_ext@moneylion.com>
2025-07-30 14:45:46 -0300

    front

---
+++ frontend/AdamChat/client/src/App.tsx
@@ -0,0 +1,36 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/components/theme-provider";
+import NotFound from "@/pages/not-found";
+import ProjectDashboard from "./pages/project-dashboard";
+import Chat from "@/pages/chat";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={ProjectDashboard} />
+      <Route path="/projects" component={ProjectDashboard} />
+      <Route path="/project/:projectId" component={Chat} />
+      <Route path="/project/:projectId/chat/:conversationId?" component={Chat} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
