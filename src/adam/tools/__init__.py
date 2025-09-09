6a4015f544ddea73eac3e8c9532476cdd5302e44
vitoryago <vcarvalho_ext@moneylion.com>
2025-09-08 21:15:09 -0300

    init

--- src/adam/tools/__init__.py
+++ src/adam/tools/__init__.py
@@ -1,8 +1,38 @@
 """
 ADAM Tools Package
-Analytics-focused tools for SQL, dbt, and data engineering
+External capabilities for enhanced functionality
 """
 
 from .sql_tools import SQLAnalyzer, SQLFormatter, SQLOptimizer
 
-__all__ = ['SQLAnalyzer', 'SQLFormatter', 'SQLOptimizer']
+# New tool imports (will be added as we create them)
+try:
+    from .web_search import WebSearchTool
+except ImportError:
+    WebSearchTool = None
+
+# Better approach - use model's native web search
+try:
+    from .model_web_search import ModelWebSearch, ADAMWebSearchIntegration
+except ImportError:
+    ModelWebSearch = None
+    ADAMWebSearchIntegration = None
+
+try:
+    from .code_executor import CodeExecutor
+except ImportError:
+    CodeExecutor = None
+
+try:
+    from .file_generator import FileGenerator
+except ImportError:
+    FileGenerator = None
+
+__all__ = [
+    'SQLAnalyzer', 
+    'SQLFormatter', 
+    'SQLOptimizer',
+    'WebSearchTool',
+    'CodeExecutor',
+    'FileGenerator'
+]
